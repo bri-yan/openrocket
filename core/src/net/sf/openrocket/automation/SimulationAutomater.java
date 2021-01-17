0c4cd479046133bcca174a22efd1e63641c9cb9d
ishwarg <67804752+ishwarg@users.noreply.github.com>
2021-01-17 13:10:44 -0800

    Update SimulationAutomater.java

--- core/src/net/sf/openrocket/automation/SimulationAutomater.java
+++ core/src/net/sf/openrocket/automation/SimulationAutomater.java
@@ -34,3 +34,52 @@ public class SimulationAutomater {
 
     }
 }
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define ROOTC 30
+#define minHeight 29500
+#define maxHeight 37000
+int main() {
+	double lowerTip = 0.0;
+	double upperTip = 0.0;
+	double lowerHeight = 0.0;
+	double upperHeight = 0.0;
+	double lowerSweep = 0.0;
+	double upperSweep = 0.0;
+	double maxApogee = 0.0;
+	double Apogee = 0.0;
+	double stabOffLaunch = 0.0;
+	double stabOffSep = 0.0;
+	while (lowerTip <= 20) {
+		while (upperTip <= 20) {
+			while (lowerHeight <= 20) {
+				while(upperHeight <= 20) {
+					while (lowerSweep <= 20) {
+						while (upperSweep <= 20) {
+							if (stabOffLaunch < 1.6 && stabOffSep >= 2.25) { //stability right after stage sep should be greater than or equal 2.25
+								if (upperTip + upperSweep >= ROOTC || lowerTip + lowerSweep >= ROOTC) { //maintain proper fin orientation
+									upperTip += 0.1;
+								}
+								else {
+									break;
+								}
+								if (Apogee > maxApogee)
+									if(Apogee > minHeight && Apogee < maxHeight)
+									maxApogee = Apogee;
+							}
+						}
+						upperSweep += 0.1;
+					}
+					lowerSweep += 0.1;
+				}
+				upperHeight += 0.1;
+			}
+			lowerHeight += 0.1;
+		}
+		upperTip += 0.1;
+	}
+	lowerTip += 0.1;
+} 
